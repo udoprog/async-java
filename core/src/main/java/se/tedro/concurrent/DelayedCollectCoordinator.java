5c8fe3a5dde321e38fa087275fff41644f91fde4
John-John Tedro <udoprog@tedro.se>
2017-03-12 07:39:17 +0100

    [core] improve DelayedCollectCoordinator

--- core/src/main/java/se/tedro/concurrent/DelayedCollectCoordinator.java
+++ core/src/main/java/se/tedro/concurrent/DelayedCollectCoordinator.java
@@ -1,9 +1,10 @@
 package se.tedro.concurrent;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -14,102 +15,95 @@ import java.util.function.Supplier;
  * @param <T> The target type the source type is being collected into.
  */
 public class DelayedCollectCoordinator<S, T> implements Handle<S>, Runnable {
-  private final AtomicInteger pending = new AtomicInteger();
-
   /* lock that must be acquired before using {@link callables} */
   private final Object lock = new Object();
 
   private final Caller caller;
-  private final Iterator<? extends Callable<? extends Stage<? extends S>>> callables;
+  private final Iterator<? extends Callable<? extends Stage<? extends S>>> tasks;
   private final Consumer<? super S> consumer;
   private final Supplier<? extends T> supplier;
   private final Completable<? super T> future;
   private final int parallelism;
-  private final int total;
 
   volatile boolean cancel = false;
   volatile boolean done = false;
+  volatile int pending = 0;
 
   public DelayedCollectCoordinator(
-    final Caller caller,
-    final Collection<? extends Callable<? extends Stage<? extends S>>> callables,
+    final Caller caller, final Collection<? extends Callable<? extends Stage<? extends S>>> tasks,
     final Consumer<S> consumer, Supplier<T> supplier, final Completable<? super T> future,
     int parallelism
   ) {
     this.caller = caller;
-    this.callables = callables.iterator();
+    this.tasks = tasks.iterator();
     this.consumer = consumer;
     this.supplier = supplier;
     this.future = future;
     this.parallelism = parallelism;
-    this.total = callables.size();
   }
 
   @Override
   public void failed(Throwable cause) {
-    pending.decrementAndGet();
-    cancel = true;
-    checkNext();
+    next(() -> {
+      pending--;
+      cancel = true;
+    });
   }
 
   @Override
   public void completed(S result) {
     caller.execute(() -> consumer.accept(result));
-    pending.decrementAndGet();
-    checkNext();
+    next(() -> pending--);
   }
 
   @Override
   public void cancelled() {
-    pending.decrementAndGet();
-    cancel = true;
-    checkNext();
+    next(() -> {
+      pending--;
+      cancel = true;
+    });
   }
 
   // coordinate thread.
   @Override
   public void run() {
+    final List<Callable<? extends Stage<? extends S>>> initial = new ArrayList<>();
+
     synchronized (lock) {
-      if (!callables.hasNext()) {
-        checkEnd();
-        return;
+      for (int i = 0; i < parallelism && tasks.hasNext(); i++) {
+        initial.add(tasks.next());
       }
 
-      for (int i = 0; i < parallelism && callables.hasNext(); i++) {
-        setupNext(callables.next());
-      }
+      pending += initial.size();
     }
 
+    initial.forEach(this::call);
+
     future.whenCancelled(() -> {
-      cancel = true;
-      checkNext();
+      next(() -> cancel = true);
     });
   }
 
-  private void checkNext() {
+  private void next(final Runnable effect) {
     final Callable<? extends Stage<? extends S>> next;
 
     synchronized (lock) {
-      if (cancel) {
-        checkEnd();
+      effect.run();
+
+      if (cancel || !tasks.hasNext()) {
+        end();
         return;
       }
 
-      if (!callables.hasNext()) {
-        checkEnd();
-        return;
-      }
-
-      next = callables.next();
+      pending++;
+      next = tasks.next();
     }
 
-    setupNext(next);
+    call(next);
   }
 
-  private void setupNext(final Callable<? extends Stage<? extends S>> next) {
+  private void call(final Callable<? extends Stage<? extends S>> next) {
     final Stage<? extends S> f;
-
-    pending.incrementAndGet();
 
     try {
       f = next.call();
@@ -121,26 +115,22 @@ public class DelayedCollectCoordinator<S, T> implements Handle<S>, Runnable {
     f.handle(this);
   }
 
-  private void checkEnd() {
-    if (pending.get() > 0) {
-      return;
-    }
-
-    if (done) {
+  private void end() {
+    if (pending > 0 || done) {
       return;
     }
 
     done = true;
 
-    final T value;
+    final T result;
 
     try {
-      value = supplier.get();
-    } catch (Exception e) {
+      result = supplier.get();
+    } catch (final Exception e) {
       future.fail(e);
       return;
     }
 
-    future.complete(value);
+    future.complete(result);
   }
 }
